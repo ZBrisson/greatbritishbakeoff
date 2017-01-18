c557f24b445a895e6acd9dcdb3cb9a716dba5067
Zach Brisson <github@zachbrisson.com>
2017-01-18 14:30:03 -0500

    Update britishBakeOff.js
    Fixed bug. Random number was generating from the index of a blank array.

--- britishBakeOff.js
+++ britishBakeOff.js
@@ -1,14 +1,17 @@
 var bakeOffTextHolder = [];
-var randomNumber = Math.floor(Math.random()*bakeOffTextHolder.length);
+var randomNumber = [];
 
 
 $(document).ready(function() {
 	$.get('bakeoff.txt', function(data){
         bakeOffTextHolder = data.split('\n');
+        randomNumber = Math.floor(Math.random()*bakeOffTextHolder.length);
     });
 
 	$("button").click(function() {
 		$("#generatorResults").html(bakeOffTextHolder[randomNumber]);
+		console.log(bakeOffTextHolder);
+		console.log(randomNumber);
 	});
 });
 
