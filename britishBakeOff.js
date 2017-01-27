4e76dd784435ec4890da091b9e0c06f3c7c2f294
ZBrisson <github@zachbrisson.com>
2017-01-26 21:56:18 -0500

    Replaced .txt with .json and .css

--- britishBakeOff.js
+++ britishBakeOff.js
@@ -1,17 +1,22 @@
-var bakeOffTextHolder = [];
-var randomNumber = [];
-
+var bakeOffTextHolder = JSON.parse(data);
+var selectedRecipe = [];
+console.log("start"); //Unit Test
+console.log(bakeOffTextHolder); //Unit Test
 
 $(document).ready(function() {
-	$.get('bakeoff.txt', function(data){
-        bakeOffTextHolder = data.split('\n');
-        randomNumber = Math.floor(Math.random()*bakeOffTextHolder.length);
-    });
+
+	function pickRandomRecipe(){
+		var obj_keys = Object.keys(bakeOffTextHolder);
+		var ran_key = obj_keys[Math.floor(Math.random() *obj_keys.length)];
+		selectedRecipe = bakeOffTextHolder[ran_key];
+		console.log(selectedRecipe.name); //Unit Test
+	}
 
 	$("button").click(function() {
-		$("#generatorResults").html(bakeOffTextHolder[randomNumber]);
-		console.log(bakeOffTextHolder);
-		console.log(randomNumber);
+		pickRandomRecipe();
+		$("#generatorResults").html(selectedRecipe.name);
+		$("#generatorResults").show();
+		console.log(selectedRecipe.id); //Unit Test
+		console.log("finish"); //Unit Test
 	});
 });
-
